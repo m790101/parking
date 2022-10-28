88cc1a36303da09c24eacd1db1821e1f2c37ea32
m790101 <m790101@hotmail.com>
2022-10-28 22:05:16 +0800

    add google map api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,40 @@
-const Home = ()=>{
-    return (
+import React from 'react'
+import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api'
+//import { useCallback, useState, useRef, useEffect } from 'react'
+//import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
+//import axios from 'axios'
+const libraries = ["places"];
+const containerStyle = { width: '100vw', height: '100vh' }
+let center = { lat: 25.03, lng: 121.554 }
+const Map = ()=>{
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+        libraries
+      })
+      console.log(process.env.REACT_APP_GOOGLE_MAP_API_KEY)
+      if (!isLoaded) {
+        return (
+          <h1>Loading...</h1>
+        )
+      }
+      return (
         <div>
+
+          <GoogleMap center={center}
+            zoom={13}
+            mapContainerStyle={containerStyle}
+            options={{
+              streetViewControl: false,
+              mapTypeControl: false,
+              fullScreenControl: false,
+              disableDefaultUI: true,
+              zoomControl: true,
+            }}
+          >
+          </GoogleMap>
         </div>
-    )
+      )
 }
 
-export default Home
+export default Map
