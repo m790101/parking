d93c38e872f0d1064b77be443f8444585da9e276
m790101 <m790101@hotmail.com>
2022-11-15 20:29:09 +0800

    add twd97 to lan and lng also fetch real data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React, {  } from 'react'
 import './../style/home.scss'
-import { useJsApiLoader, GoogleMap, Marker, DirectionsRenderer } from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, Marker, DirectionsRenderer, MarkerClusterer } from '@react-google-maps/api'
 import { useCallback, useState, useRef, useEffect } from 'react'
 import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
 import mapStyle from '../mapStyle'
 import Details from '../components/Details'
 import axios from 'axios'
-import db from '../db.json'
-import available from '../available.json'
 import Navbar from '../components/Navbar'
 import OpenDisplay from '../components/OpenDisplay'
 import Help from '../components/Help'
@@ -16,6 +14,15 @@ import Report from '../components/Report'
 import Locate from '../components/Locate'
 import Search from '../components/Search'
 import { isInRange } from '../utils/isInRange'
+import { twd97ToLatlng } from '../utils/twd97ToLatlng'
+
+let parkingData = "https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_alldesc.json"
+let availbilityData = "https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_allavailable.json"
+ 
+const requestParking = axios.get(parkingData);
+const requestAvailbility = axios.get(availbilityData);
+
+
 const options = {
   enableHighAccuracy: false,
   timeout: 1000,
@@ -26,51 +33,15 @@ const markerIcon = {
   yellow: 'https://i.imgur.com/lKDCX1d.png',
   red: 'https://i.imgur.com/M7l0UWq.png'
 }
-let availableDb = available.data.park
-let parkingDb = db.data.park
-parkingDb = parkingDb.map((park) => {
-  let a = availableDb.filter(a => park.id === a.id)
-  const day = new Date().getDay()
-  const timeOrigin = new Date().getHours()
-  const time = Number(timeOrigin) < 10 ? '0' + timeOrigin + ':00':timeOrigin + ':00'
-  let fare = ''
-  if (park.FareInfo.WorkingDay) {
-    if (day > 0 && day <= 5) {
-      for (let i = 0; i < park.FareInfo.WorkingDay.length; i++) {
-        let timeFix = park.FareInfo.WorkingDay[i].Period.split('~')
-        if (isInRange(time, timeFix)) {
-          fare = park.FareInfo.WorkingDay[i].Fare
 
-        }
-      }
-    }
-    if (day === 0 || day === 6) {
-      for (let i = 0; i < park.FareInfo.Holiday.length; i++) {
-        let timeFix = park.FareInfo.Holiday[i].Period.split('~')
-        if (isInRange(time, timeFix)) {
-          fare = park.FareInfo.Holiday[i].Fare
-
-        }
-      }
-    }
-  }
-
-  return {
-    ...park,
-    lat: Number(park.EntranceCoord.EntrancecoordInfo[0].Xcod),
-    lng: Number(park.EntranceCoord.EntrancecoordInfo[0].Ycod),
-    cap: (a[0].availablecar / park.totalcar),
-    fare
-  }
-})
 let center = { lat: 25.03, lng: 121.554 }
 let watchId = null
+
 const Map = () => {
   const [libraries] = useState(['places'])
   let [parkingMarkers, setParkingMarkers] = useState([])
   const [currentMarker, setCurrentMarkers] = useState([])
   const [searchMarkers, setSearchMarkers] = useState([])
-  const [parkingData, setParkingData] = useState([])
   const [availbility, setAvailbility] = useState([])
   const [directionResponse, setDirectionResponse] = useState(null)
   const [duration, setDuration] = useState('')
@@ -81,12 +52,12 @@ const Map = () => {
   const [isReporting, setIsReporting] = useState(null)
   //const [intialPostition,setIntialPostition] = useState({ lat: 0, lng: 0 })
   const mapRef = useRef()
-
+  //let testMarkers= useMemo( db.data.park)
   const onMapLoad = useCallback(map => {
     mapRef.current = map
     const bounds = new window.google.maps.LatLngBounds();
     bounds.extend(center)
-    bounds.extend({ lat: center.lat + 0.02, lng: center.lng + 0.02})
+    bounds.extend({ lat: center.lat + 0.02, lng: center.lng + 0.02 })
     map.fitBounds(bounds);
 
   }, [])
@@ -97,27 +68,31 @@ const Map = () => {
     mapRef.current.setZoom(16);
 
   }, []);
-  const initialMarkers = useCallback((p, lat, lng, availableDb, fare) => {
+  /*const initialMarkers = useCallback((p, lat, lng, fare) => {
     setParkingMarkers((currents) => {
       currents = currents.filter(current => current.id !== p.id)
-      let a = availableDb.filter(a => p.id === a.id)
+      //let a = availableDb.filter(a => p.id === a.id)
       return [...currents, {
         ...p,
         lat,
         lng,
-        cap: (a[0].availablecar / p.totalcar),
         fare
       }]
     })
-  }, [])
+  }, [])*/
+  const initialMarkers = useCallback((data) => setParkingMarkers(data),[])
+
+  /*const initialMarkers = useMemo((data) => {
+    setParkingMarkers(data)}, [])
+*/
 
   const initialLocate = useCallback(() => {
- 
-    navigator.geolocation.getCurrentPosition((position)=>{
-        panTo({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      panTo({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
     })
     watchId = navigator.geolocation.watchPosition(
       (position) => {
@@ -128,20 +103,29 @@ const Map = () => {
         }])
         setIsLoading(null)
       },
-      () => null,options
+      () => null, options
     )
   }, [panTo])
 
   useEffect(() => {
-    axios.get('https://my-json-server.typicode.com/m790101/parking-data/data')
-      .then(data => {
+    axios.all([requestParking,requestAvailbility])
+      .then(datas => {
         navigator.geolocation.clearWatch(watchId)
-        setParkingData(parkingDb)
-        setAvailbility(availableDb)
-        data.data.park.map(async (p) => {
+    
+        let parkingAvailability = datas[1].data.data.park.map((a)=>{
+          return {
+            ...a,
+            availablecar:a.availablecar < 0?'無資料':a.availablecar,
+          }
+        })
+        setAvailbility(parkingAvailability)
+        let parkingLot = datas[0].data.data.park.map((p) => {
+          let a = datas[1].data.data.park.filter(a => p.id === a.id)
+          let lat = Number(twd97ToLatlng(p.tw97x, p.tw97y).lat)
+          let lng = Number(twd97ToLatlng(p.tw97x, p.tw97y).lng)
           const day = new Date().getDay()
           const timeOrigin = new Date().getHours()
-          const time = Number(timeOrigin) < 10 ? '0' + timeOrigin + ':00':timeOrigin + ':00'
+          const time = Number(timeOrigin) < 10 ? '0' + timeOrigin + ':00' : timeOrigin + ':00'
           let fare = ''
           if (p.FareInfo.WorkingDay) {
             if (day > 0 && day <= 5) {
@@ -162,22 +146,27 @@ const Map = () => {
               }
             }
           }
-          const response = await getGeocode({ address: p.address })
-          const { lat, lng } = await getLatLng(response[0]);
-          initialMarkers(p, lat, lng, availableDb, fare)
+          //initialMarkers(p, lat, lng, fare)
+          return {
+            ...p,
+          lat,
+          lng,
+          fare: fare ,
+          availablecar: a.length > 0 ? a[0].availablecar:"無資料"
+          }
+          
         })
-
+        return parkingLot
       }
       )
-      .then(() => {
-
+      .then((data) => {
+        //initialMarkers(data)
+        initialMarkers(data)
+        //setParkingMarkers(data)
         initialLocate()
       })
-      .then(() => { })
-      return navigator.geolocation.clearWatch(watchId)
+    return navigator.geolocation.clearWatch(watchId)
   }, [initialLocate, initialMarkers])
-
-
 
 
 
@@ -257,39 +246,45 @@ const Map = () => {
               anchor: new window.google.maps.Point(15, 11)
             }}
           />)}
-          {parkingData.map(marker => <Marker
-            key={marker.id}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            icon={{
-              url: marker.cap === 0 ? markerIcon.red : marker.cap > 0.1 ? markerIcon.black : markerIcon.yellow,
-              scaledSize: new window.google.maps.Size(30, 30),
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 11)
-            }}
-            label={{ text: marker.fare ? '$'+marker.fare : ' ', className: 'marker-label' }}
-            onClick={() => {
-              setSelected(marker)
-              setIsHelp(false)
-              fetchDirections(marker)
-            }}
-          />)}
-
-          {parkingMarkers.map(marker => <Marker
-            key={marker.id}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            icon={{
-              url: marker.cap === 0 ? markerIcon.red : marker.cap > 0.1 ? markerIcon.black : markerIcon.yellow,
-              scaledSize: new window.google.maps.Size(30, 30),
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 11)
-            }}
-            label={{ text: '$'+marker.fare, className: 'marker-label' }}
-            onClick={() => {
-              setSelected(marker)
-              setIsHelp(false)
-              fetchDirections(marker)
-            }}
-          />)}
+          
+          <MarkerClusterer >
+            {(clusterer) => parkingMarkers.map(marker => <Marker
+                key={marker.id}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                icon={{
+                  url: markerIcon.black,
+                  scaledSize: new window.google.maps.Size(30, 30),
+                  origin: new window.google.maps.Point(0, 0),
+                  anchor: new window.google.maps.Point(15, 11)
+                }}
+                label={{ text: '$' + marker.fare, className: 'marker-label' }}
+                clusterer={clusterer}
+                onClick={() => {
+                  setSelected(marker)
+                  setIsHelp(false)
+                  fetchDirections(marker)
+                }}
+              />)}
+          </MarkerClusterer>
+          <MarkerClusterer >
+            {(clusterer) => parkingMarkers.map(marker => <Marker
+                key={marker.id}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                icon={{
+                  url: markerIcon.black,
+                  scaledSize: new window.google.maps.Size(30, 30),
+                  origin: new window.google.maps.Point(0, 0),
+                  anchor: new window.google.maps.Point(15, 11)
+                }}
+                label={{ text: '$' + marker.fare, className: 'marker-label' }}
+                clusterer={clusterer}
+                onClick={() => {
+                  setSelected(marker)
+                  setIsHelp(false)
+                  fetchDirections(marker)
+                }}
+              />)}
+          </MarkerClusterer>
           {navigate && <DirectionsRenderer directions={directionResponse} options={{ suppressMarkers: true }} />}
         </GoogleMap>
 
